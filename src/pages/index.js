1b3c4d8447ad7dd796bc9f2dc454a836d28f53c6
Muhammad Ali Farooqi <m.ali_farooqi@hotmail.com>
2018-09-13 12:00:47 +0800

    testing auto deploy with some changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>I am a third-year Computer Science student.</p>
     <p>And I am about to build something great...</p>
+    <p>You can visit my <a href="https://alifarooqi.github.io/" target="_blank">portfolio</a> to get to know more about me.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,7 +3,7 @@ import Link from 'gatsby-link'
 
 const SecondPage = () => (
   <div>
-    <h1>Hi once again</h1>
+    <h1>Hello once again</h1>
     <p>You can visit my <a href="https://alifarooqi.github.io/" target="_blank">portfolio</a> to get to know more about me.</p>
     <Link to="/">Go back to the homepage</Link>
   </div>

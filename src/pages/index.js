6970f35b62f71f40f2c1618419e006beb7b986be
Muhammad Ali Farooqi <m.ali_farooqi@hotmail.com>
2018-09-13 11:46:26 +0800

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>I am a third-year Computer Science student.</p>
+    <p>And I am about to build something great...</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
---
+++ src/pages/page-2.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const SecondPage = () => (
+  <div>
+    <h1>Hi once again</h1>
+    <p>You can visit my <a href="https://alifarooqi.github.io/" target="_blank">portfolio</a> to get to know more about me.</p>
+    <Link to="/">Go back to the homepage</Link>
+  </div>
+)
+
+export default SecondPage
